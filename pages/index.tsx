cc3aba60eda1f880da5c519c0cee909695e7a43e
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-02-07 02:29:26 +0530

    Add some basic info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage, NextPageContext } from 'next';
+import type { NextPage } from 'next';
 import {
   Box,
   Button,
@@ -6,6 +6,10 @@ import {
   Divider,
   Heading,
   HStack,
+  Link,
+  List,
+  ListItem,
+  Text,
   useDisclosure,
   VStack,
 } from '@chakra-ui/react';
@@ -13,6 +17,18 @@ import { Web3Modal } from '../components/Web3Modal';
 import { Entry, getEntries } from './api/entries';
 import { Account } from '../components/Account';
 import { useState } from 'react';
+
+const MyLink = ({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) => (
+  <Link href={href} target='_blank' color='green.300'>
+    {children}
+  </Link>
+);
 
 const useEntries = () => {
   const [entries, setEntries] = useState<Entry[]>();
@@ -39,11 +55,21 @@ const Home: NextPage<HomeProps> = ({ entries }) => {
   const data = latestEntries || entries;
 
   return (
-    <Container pt={20}>
+    <Container py={20}>
+      <Web3Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        fetchEntries={fetchEntries}
+      />
       <VStack spacing={8}>
-        <Box textAlign='center'>
+        <Box
+          textAlign='center'
+          display='flex'
+          flexDirection='column'
+          gap='20px'
+        >
           <Heading as='h1' size='xl'>
-            Dhaiwat Pandya
+            Dhaiwat Pandya 🚀
           </Heading>
           <Heading
             as='a'
@@ -54,13 +80,68 @@ const Home: NextPage<HomeProps> = ({ entries }) => {
           >
             dhaiwat.eth
           </Heading>
+          <HStack marginX='auto'>
+            <Button
+              variant='outline'
+              as='a'
+              href='https://twitter.com/dhaiwat10'
+              target='_blank'
+            >
+              Twitter
+            </Button>
+            <Button
+              variant='outline'
+              as='a'
+              href='https://github.com/dhaiwat10'
+              target='_blank'
+            >
+              Github
+            </Button>
+            <Button
+              variant='outline'
+              as='a'
+              href='https://mirror.xyz/dhaiwat.eth'
+              target='_blank'
+            >
+              Mirror
+            </Button>
+          </HStack>
+          <Text>
+            Web3 frontend engineer at{' '}
+            <MyLink href='https://mazury.xyz'>Mazury.</MyLink> Building free,
+            open-source software with{' '}
+            <MyLink href='https://twitter.com/developer_dao'>
+              Developer DAO
+            </MyLink>{' '}
+            and{' '}
+            <MyLink href='https://twitter.com/moonshotcollect'>
+              the Moonshot Collective.
+            </MyLink>
+          </Text>
+
+          <VStack>
+            <Heading as='h3' size='md'>
+              My best work
+            </Heading>
+            <List>
+              <ListItem>
+                <MyLink href='https://github.com/developer-dao/web3-ui'>
+                  web3-ui
+                </MyLink>
+              </ListItem>
+              <ListItem>
+                <MyLink href='https://github.com/developer-dao/web3-ui'>
+                  A guide to Web3 for Web2 frontend devs
+                </MyLink>
+              </ListItem>
+              <ListItem>
+                <MyLink href='https://github.com/dhaiwat10/react-link-preview'>
+                  react-link-preview
+                </MyLink>
+              </ListItem>
+            </List>
+          </VStack>
         </Box>
-        <Button onClick={onOpen}>Sign my guestbook</Button>
-        <Web3Modal
-          isOpen={isOpen}
-          onClose={onClose}
-          fetchEntries={fetchEntries}
-        />
         <Divider />
         <Box textAlign='center'>
           <Heading size='lg'>📓 Guestbook entries</Heading>
@@ -68,6 +149,7 @@ const Home: NextPage<HomeProps> = ({ entries }) => {
             These kind people said gm.
           </Heading>
         </Box>
+        <Button onClick={onOpen}>Sign my guestbook</Button>
         <VStack
           width={{
             sm: '100%',
@@ -80,18 +162,6 @@ const Home: NextPage<HomeProps> = ({ entries }) => {
             <Account key={entry.address} address={entry.address} />
           ))}
         </VStack>
-
-        <HStack>
-          <Button as='a' href='https://twitter.com/dhaiwat10' target='_blank'>
-            Twitter
-          </Button>
-          <Button as='a' href='https://github.com/dhaiwat10' target='_blank'>
-            Github
-          </Button>
-          <Button as='a' href='https://mirror.xyz/dhaiwat.eth' target='_blank'>
-            Mirror
-          </Button>
-        </HStack>
       </VStack>
     </Container>
   );
