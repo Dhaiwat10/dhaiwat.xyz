3d9b042f088b8ef978d9fa1f00ed9ab7d9c9f721
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-06-18 15:31:53 +0530

    feat: 🎸 start working on nft minting feature

--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -2,6 +2,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { verifyMessage } from 'ethers/lib/utils';
 import { createClient } from '@supabase/supabase-js';
+import { ThirdwebSDK } from '@thirdweb-dev/sdk';
+import { ethers } from 'ethers';
 
 const supabase = createClient(
   'https://tpuycrkgdcqwcwrihmjj.supabase.co',
@@ -10,6 +12,7 @@ const supabase = createClient(
 
 type Data = {
   address: string;
+  tokenId: string;
 };
 
 type Error = {
@@ -20,18 +23,46 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
-  console.log({ msg: req.query.message });
+  const provider = new ethers.providers.JsonRpcProvider(
+    'https://rpc.ankr.com/polygon_mumbai',
+    80001
+  );
+  const sdk = new ThirdwebSDK(provider, {
+    gasless: {
+      openzeppelin: {
+        relayerUrl: process.env.RELAYER_WEBHOOK_URL as string,
+      },
+    },
+  });
+  const nftContract = sdk.getNFTCollection(
+    '0x24bB3E4C3b2611976aA5494790Bd74e20F4CdeD3'
+  );
+
   const message = req.query.message as string;
   const recoveredAddress = verifyMessage('gm', message);
-  console.log({ recoveredAddress });
+
   const { error } = await supabase
     .from('entries')
     .insert([{ address: recoveredAddress }]);
-  if (error) {
+
+  if (process.env.NODE_ENV === 'production' && error) {
     if (error.code === '23505') {
       return res.status(400).json({ error: 'You already have an entry.' });
     }
     return res.status(500).json({ error: error.message });
   }
-  return res.status(200).json({ address: recoveredAddress });
+
+  const metadata = {
+    name: `Thanks for signing dhaiwat.xyz's guestbook! <3`,
+    description: 'Dhaiwat loves you',
+    image:
+      'https://yvserbqzlsygbs4alzsbfautd3q7exfqwwflgzrspqikcdep.arweave.net/xWRI_hhlcsGDLgF5kEoKTHuHyXLC1irNmMnw_QoQyPU',
+  };
+
+  const tx = await nftContract.mintTo(recoveredAddress, metadata);
+  const tokenId = tx.id.toString();
+
+  console.log(tx, tokenId);
+
+  return res.status(200).json({ address: recoveredAddress, tokenId });
 }
