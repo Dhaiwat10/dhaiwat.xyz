78ef6b7e0b02a80797eea070fc8efa71d9c43512
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2022-08-15 21:46:23 +0530

    update cv

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -17,13 +17,14 @@ const CVPage: NextPage = () => {
       <VStack textAlign='left' alignItems='start' spacing='4'>
         <Heading>Dhaiwat Pandya</Heading>
         <Text>
-          21 year old frontend engineer and content creator. Currently working
-          as a frontend engineer at{' '}
-          <MyLink href='https://mazury.xyz'>mazury.xyz</MyLink>.<br />
+          21 year old devrel and engineer. Currently working as a devrel at{' '}
+          <MyLink href='https://ankr.com'>Ankr</MyLink>. Prev frontend engineer
+          at <MyLink href='https://mazury.xyz'>Mazury</MyLink>.
+          <br />
           <MyLink href='https://twitter.com/developer_dao'>
             Developer DAO
           </MyLink>{' '}
-          core contributor &amp; Development Guild co-lead.
+          core contributor
         </Text>
 
         <VStack textAlign='left' alignItems='start'>
@@ -44,13 +45,10 @@ const CVPage: NextPage = () => {
           technical content I created around React and web3. This includes my
           articles, my tweets and my talks.
           <br />
-          <br />
-          On average, my tweets get around <b>~700k impressions</b> each month
-          and each of my articles get around <b>~10k reads</b>.
         </Text>
 
         <Text>
-          As a devrel I want to continue creating quality content and helping
+          As a devrel, I want to continue creating quality content and helping
           people get into web3. I have already done this through my articles &
           tweets. I want to continue doing so on a more regular basis. With a
           strong technical background in the Javascript/TypeScript ecosystem,
@@ -67,13 +65,18 @@ const CVPage: NextPage = () => {
             <MyLink href='https://mirror.xyz/dhaiwat.eth/O5CK6Tjfv8uhl6FPbjT0yZ8LUwViDPWGYHdu9khRWpM'>
               A guide to web3 for web2 frontend devs
             </MyLink>
-            : This is my best written content to date. This article got
-            RT&apos;d by many &apos;big&apos; accounts including @ethereum and
-            Brendan Eich (creator of JavaScript). I&apos;m extremely proud of
-            this one.
+            : This article helped thousands of people. Also RT&apos;d by many
+            &apos;major&apos; accounts including the official Ethereum account.
+            I&apos;m extremely proud of this one.
             <br />
-            Some numbers: <b>180k</b> tweet impressions, <b>10k</b> reads
-            <br />
+          </ListItem>
+
+          <ListItem>
+            <MyLink href='https://ankr.hashnode.dev/build-and-deploy-a-rentable-nft-smart-contract-on-optimism'>
+              Build and Deploy A Rentable NFT collection
+            </MyLink>
+            : The first piece of content around ERC4907 - a recently accepted
+            standard for rentable NFTs.
             <br />
           </ListItem>
 
@@ -82,8 +85,19 @@ const CVPage: NextPage = () => {
               web3cheatsheet.xyz
             </MyLink>
             : A set of useful code snippets and mini-guides to help you build
-            web3 apps. I wrote all the content on this site and published it
-            very recently.
+            web3 apps. I wrote all the content on this site.
+          </ListItem>
+
+          <ListItem>
+            <MyLink href='https://ankr.hashnode.dev/build-an-nft-minting-page-with-rainbowkit-wagmi'>
+              Build an NFT Minting Page with Rainbowkit &amp; wagmi
+            </MyLink>
+          </ListItem>
+
+          <ListItem>
+            <MyLink href='https://mirror.xyz/dhaiwat.eth/NV--7dv8CO0NCcFCvRjDCxBe3VuxdB2_KggwFEfLGRc'>
+              Permanently deploy decentralized React frontends to Arweave
+            </MyLink>
           </ListItem>
         </UnorderedList>
 
@@ -93,7 +107,7 @@ const CVPage: NextPage = () => {
             <MyLink href='https://dev.to/dhaiwat10/develop-test-react-components-in-isolation-3714'>
               Develop &amp; test React components in isolation
             </MyLink>
-            : This article kickstarted my journey as a &apos;writer&apos;.
+            : This article kickstarted my writing journey.
           </ListItem>
 
           <ListItem>
@@ -128,15 +142,34 @@ const CVPage: NextPage = () => {
 
         <UnorderedList>
           <ListItem>
-            <MyLink href='https://youtu.be/_JuLIGeXsxI?t=9721'>
-              Shipping robust &amp; maintainable React packages to npm - talk at
-              React India 2021
+            <MyLink href='https://www.youtube.com/watch?v=3QgqoHggwhg'>
+              Sign-in with Ethereum Tutorial w/ React &amp; wagmi
+            </MyLink>
+          </ListItem>
+
+          <ListItem>
+            <MyLink href='https://www.youtube.com/watch?v=CaODHRnFgBw'>
+              The Easiest Way To Deploy Smart Contracts
             </MyLink>
           </ListItem>
 
           <ListItem>
             <MyLink href='https://www.youtube.com/watch?v=lk02X2vfUnQ'>
               How I started my web3 OSS journey with D_D - talk at web3con 2022
+            </MyLink>
+          </ListItem>
+
+          <ListItem>
+            <MyLink href='https://twitter.com/dhaiwat10/status/1558813322266480640?s=20&t=LETYN5mfhJmP4vvby2ptjA'>
+              The state of web3 frontends - keynote at Web3Conf India 2022
+              (video not available yet)
+            </MyLink>
+          </ListItem>
+
+          <ListItem>
+            <MyLink href='https://youtu.be/_JuLIGeXsxI?t=9721'>
+              Shipping robust &amp; maintainable React packages to npm - talk at
+              React India 2021
             </MyLink>
           </ListItem>
 
@@ -192,11 +225,11 @@ const CVPage: NextPage = () => {
             <MyLink href='https://github.com/mazurylabs/mazury-frontend'>
               Mazury
             </MyLink>
-            : This is my current job. Mazury has indexed the blockchain so that
-            recruiters can find talent easily based on their on-chain
-            credentials. I joined Mazury in January &apos;22 and basically
-            created this from scratch. I designed the entire architecture and am
-            now leading our frontend team.
+            : Mazury has indexed the blockchain so that recruiters can find
+            talent easily based on their on-chain credentials. I joined Mazury
+            in January &apos;22 and basically created this from scratch. I
+            designed the entire architecture and was leading the frontend team
+            by the time I left.
           </ListItem>
 
           <ListItem>
